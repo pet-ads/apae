d3d13cbb1037afb8adc14b559fe5bcb15e80f4ae
biankyrou <111410892+biankyrou@users.noreply.github.com>
2023-05-09 22:43:48 -0300

    Update home.js

--- src/paginas/Home/home.js
+++ src/paginas/Home/home.js
@@ -1,19 +1,19 @@
 import "./home.css";
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return(
         <body>
             <section className="home-container">
+            <div className="img-home">
+                    <img src= "imagem-home-apae.jpg" alt="Img_Home"/>
+                </div>
                 <div className="home-titulo-principal-container">
                     <h1 className="home-titulo-principal">Atuando pela inclusão digital: uma parceria entre o IFSP e a APAE</h1>
                 </div>
-                <button className="button-home">Conheça o projeto</button>
-                <div className="img-home">
-                    <img src= "imagem-home-apae.jpg" alt="Img_Home"/>
-                </div>
+                <Link to="/projeto" className="button-home" as="button">Conheça o projeto</Link>
             </section>
         </body>
-    )
+    )  
 }
