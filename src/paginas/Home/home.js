4b54c84bc429d8d1ab0211d14614f475c038911f
Bianca <bianca.carvalio@aluno.ifsp.edu.br>
2023-05-09 21:58:04 -0300

    Atualizar estilos e conteúdo da página Home

--- src/paginas/Home/home.js
+++ src/paginas/Home/home.js
@@ -1,11 +1,18 @@
 import "./home.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function Home() {
     return(
         <body>
             <section className="home-container">
-                <h1 className="home-titulo-principal">Projeto APAE</h1>
-                <p>HOME</p>
+                <div className="home-titulo-principal-container">
+                    <h1 className="home-titulo-principal">Atuando pela inclusão digital: uma parceria entre o IFSP e a APAE</h1>
+                </div>
+                <button className="button-home">Conheça o projeto</button>
+                <div className="img-home">
+                    <img src= "imagem-home-apae.jpg" alt="Img_Home"/>
+                </div>
             </section>
         </body>
     )
