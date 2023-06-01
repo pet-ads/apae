ef159fbb8372673a52c0673bbfc23ae53c88e2d0
Bianca <bianca.carvalio@aluno.ifsp.edu.br>
2023-06-01 04:18:15 -0300

    blog-com-css

--- src/paginas/5-Blog/blog.js
+++ src/paginas/5-Blog/blog.js
@@ -1,10 +1,16 @@
-import "./blog.css"
+import "./blog.css";
+import img_blog from "../5-Blog/blogger.png";
 
 export default function Blog(){
     return(
         <section className="blog-container">
-            <h1>Blog</h1>
-            <p>Conteúdo da página</p>
+            <p className="paragrafo-blog">As atividades dos alunos são feitas semanalmente. Acesse o seguinte link para vê-las:</p> 
+            <div className="imagem-blog">
+                <a href="https://inclusaoifspsc.blogspot.com/">
+                    <img src={img_blog} className="imagem-link-blog"></img>
+                </a>
+            </div>
+            
         </section>
     )
 }
