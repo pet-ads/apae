f7e6a0a83fd6b6cad67bb14a2f91ce11ce107723
BBranco97 <barbarabranco1997@gmail.com>
2023-06-08 15:36:43 -0300

    alteração do tamanho dos paragrafos de projeto

--- src/paginas/1-Projeto/projeto.js
+++ src/paginas/1-Projeto/projeto.js
@@ -15,11 +15,11 @@ export default function Projeto(){
                 </div>
                 
                 <div className="descricao-projeto">
-                    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A ideia do projeto surgiu após a professora Célia, docente do IFSP, atuar como voluntária em um evento beneficiente para levantar fundos à APAE no segundo semestre de 2016.
+                    <p className="texto-proj">A ideia do projeto surgiu após a professora Célia, docente do IFSP, atuar como voluntária em um evento beneficiente para levantar fundos à APAE no segundo semestre de 2016.
                         Desde então, após visitas e conversas com os coordenadores da APAE, nasce o projeto de dar aulas de informática para os alunos.
                     </p>
 
-                    <p>&nbsp;&nbsp;&nbsp;O objetivo geral é proporcionar a pessoas com deficiência intelectual a familiarização e o aprimoramento de conhecimentos em informática,
+                    <p className="texto-proj">O objetivo geral é proporcionar a pessoas com deficiência intelectual a familiarização e o aprimoramento de conhecimentos em informática,
                         contribuindo para a sua inclusão digital e, consequentemente, sua inclusão social. Não somente o projeto tem auxiliado na inserção no mercado de trabalho,
                         mas também tem melhorado a autoestima dos alunos. O empreedimento em parceria com o IFSP e a APAE tem sido divulgado em diversos congressos e em revistas,
                         como a do Napne.
