a49efc0c2c048dd1428ae3a2ebaf156a83529d5e
Bianca <bianca.carvalio@aluno.ifsp.edu.br>
2023-06-29 22:54:59 -0300

    alterando-css-400px

--- src/paginas/5-Blog/blog.js
+++ src/paginas/5-Blog/blog.js
@@ -6,8 +6,7 @@ export default function Blog(){
         <section className="blog-container">
 
             <div className="paragrafos-geral">
-                <p className="paragrafo-blog">As atividades dos alunos são realizadas semanalmente.</p>
-                <p className="paragrafo-blog">Acesse nosso blog para saber mais:</p> 
+                <p className="paragrafo-blog">As atividades dos alunos são realizadas semanalmente. <br />Acesse nosso blog para saber mais:</p>
             </div>
 
             <div className="imagem-blog">
--- src/paginas/Home/home.js
+++ src/paginas/Home/home.js
@@ -4,26 +4,21 @@ import { Link } from 'react-router-dom';
 import Banner from './bannner.png';
 
 export default function Home({ scrollToSection }) {
-    return(
+    return (
         <div>
             <section className="home-container">
-
-                <div className="titulo-button-banner">
-
-                    <div className="titulo-button">
-                        <h1 className="home-titulo">Atuando pela inclusão digital: uma parceria entre o IFSP <br/> e a APAE</h1>
-                        <button className="button-home" onClick={() => scrollToSection("projeto")}>Conheça o Projeto</button>
-
-                    </div>
-
-                    <div className="img-home">
-                        <img src={Banner} alt="Img_Home"/>
-
-                    </div>
-
+                <div className="img-home">
+                    <img src={Banner} alt="Img_Home" />
                 </div>
-
+                <div className="container-titulo-and-button">
+                    <h1 className="home-titulo">Atuando pela inclusão digital: uma parceria entre o IFSP <br/> e a APAE</h1>
+                    <div className="titulo-button-banner">
+                        <div className="titulo-button">
+                            <button className="button-home" onClick={() => scrollToSection("projeto")}>Conheça o Projeto</button>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
-    )  
+    )
 }
