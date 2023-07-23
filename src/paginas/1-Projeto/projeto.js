4232adead52e60774664ee3d657f60265f55aef5
Bianca <bianca.carvalio@aluno.ifsp.edu.br>
2023-07-23 12:16:03 -0300

    atualizando-responsividade-2

--- src/paginas/1-Projeto/projeto.js
+++ src/paginas/1-Projeto/projeto.js
@@ -17,21 +17,13 @@ export default function Projeto(){
                 </div>
                 
                 <div className="descricao-projeto">
-                    <p className="texto-proj">A ideia do projeto surgiu após a professora Célia, docente do IFSP, atuar como voluntária em um evento beneficiente para levantar fundos à APAE no segundo semestre de 2016.
-                        Desde então, após visitas e conversas com os coordenadores da APAE, nasce o projeto de dar aulas de informática para os alunos.
+                    <p className="texto-proj">A ideia do projeto surgiu após a professora Célia, docente do IFSP, atuar como voluntária em um evento beneficiente para levantar fundos à APAE no segundo semestre de 2016. Desde então, após visitas e conversas com os coordenadores da APAE, nasce o projeto de dar aulas de informática para os alunos.
                     </p>
 
-                    <p className="texto-proj">O objetivo geral é proporcionar a pessoas com deficiência intelectual a familiarização e o aprimoramento de conhecimentos em informática,
-                        contribuindo para a sua inclusão digital e, consequentemente, sua inclusão social. 
-                    </p>
-
-                    <p className="texto-proj">
-                    Não somente o projeto tem auxiliado na inserção no mercado de trabalho,
-                        mas também tem melhorado a autoestima dos alunos. O empreedimento em parceria com o IFSP e a APAE tem sido divulgado em diversos congressos e em revistas,
+                    <p className="texto-proj">O objetivo geral é proporcionar a pessoas com deficiência intelectual a familiarização e o aprimoramento de conhecimentos em informática, contribuindo para a sua inclusão digital e, consequentemente, sua inclusão social. Não somente o projeto tem auxiliado na inserção no mercado de trabalho, mas também tem melhorado a autoestima dos alunos. O empreedimento em parceria com o IFSP e a APAE tem sido divulgado em diversos congressos e em revistas,
                         como a do Napne.
                     </p>
                 </div>
-
             </div>
         </section> 
     )
