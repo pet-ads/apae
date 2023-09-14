67e9356172b7ebf8ca7fcc9796bf5cc81de9bb11
BBranco97 <barbarabranco1997@gmail.com>
2023-09-14 12:53:12 -0300

    ajustes do modal de ações

--- src/paginas/4-Acoes/acoes.js
+++ src/paginas/4-Acoes/acoes.js
@@ -1,12 +1,22 @@
-import React from "react";
-import img_aula from '../../componentes/Card_Acoes/aula_informatica.jpg';
+import React, { useState } from 'react';
 import Card from '../../componentes/Card_Acoes/card';
+import img_aula from '../../componentes/Card_Acoes/aula_informatica.jpg';
 import img_palestra from '../../componentes/Card_Acoes/palestra.jpg';
 import img_visita from '../../componentes/Card_Acoes/visita_tecnica.jpg';
 import "./acoes.css";
-
+import Modal from '../../componentes/Modal/modal.js';
 
 export default function Acoes() {
+    const [selectedCardId, setSelectedCardId] = useState(null);
+
+    const handleOpenModal = (cardId) => {
+        setSelectedCardId(cardId);
+    }
+
+    const closeModal = () => {
+        setSelectedCardId(null);
+    }
+
     return (
         <div className="acoes-container">
             <p className="titulo-acoes">Ações</p>
@@ -16,21 +26,30 @@ export default function Acoes() {
                         title='Aulas de Informática'
                         imageUrl={img_aula}
                         body='&nbsp;&nbsp;&nbsp;No curso de informática, os alunos aprendem desde tarefas básicas, como ligar o computador, manusear o mouse e escrever no teclado, até a utilização de softwares específicos, como navegadores e aplicativos do LibreOffice.'
+                        cardId="informatica"
+                        handleOpenModal={handleOpenModal}
                     />
 
                     <Card
                         title='Visita Técnica'
                         imageUrl={img_visita}
                         body='&nbsp;&nbsp;&nbsp;Durante a visita técnica, será agendada uma data para que os alunos bolsistas e voluntários visitem a APAE e tenham a oportunidade de conhecer o espaço e o trabalho desenvolvido por essa instituição.'
+                        cardId="visita"
+                        handleOpenModal={handleOpenModal}
                     />
 
                     <Card
                         title='Palestra'
                         imageUrl={img_palestra}
                         body='&nbsp;&nbsp;&nbsp;Na palestra, um profissional da APAE é convidado para ministrar uma apresentação no IFSP sobre pessoas com deficiência intelectual.'
+                        cardId="palestra"
+                        handleOpenModal={handleOpenModal}
                     />
                 </div>
             </div>
+            <Modal cardId={selectedCardId} closeModal={closeModal} />
         </div>
-    )
+    );
 }
+    
+
