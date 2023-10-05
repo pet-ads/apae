fd585e501ac8daf445f7ceb340afeb43fab33972
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-10-05 16:30:27 -0300

    style: improve resolutions and element scaling

--- src/paginas/1-Projeto/projeto.js
+++ src/paginas/1-Projeto/projeto.js
@@ -4,23 +4,23 @@ import Imagem2 from "./labapae2.jpeg";
 
 export default function Projeto(){
     return(
-        <section className="projeto-container">
+        <section className="projeto">
 
-            <div className="projeto-titulo-container">
+            <div className="projeto__titulo">
                 <h3>Sobre nós</h3>
             </div>
 
-            <div className="conteudo">
-                <div className="img-container">
+            <div className="projeto__conteudo">
+                <div className="projeto__imagens">
                     <img src={Imagem1} alt="Alunos tendo aula de informática."/>
                     <img src={Imagem2} alt="Alunos tendo aula de informática."/>
                 </div>
                 
-                <div className="descricao-projeto">
-                    <p className="texto-proj">A ideia do projeto surgiu após a professora Célia, docente do IFSP, atuar como voluntária em um evento beneficiente para levantar fundos à APAE no segundo semestre de 2016. Desde então, após visitas e conversas com os coordenadores da APAE, nasce o projeto de dar aulas de informática para os alunos.
+                <div className="projeto__descricao">
+                    <p className="projeto__texto">A ideia do projeto surgiu após a professora Célia, docente do IFSP, atuar como voluntária em um evento beneficiente para levantar fundos à APAE no segundo semestre de 2016. Desde então, após visitas e conversas com os coordenadores da APAE, nasce o projeto de dar aulas de informática para os alunos.
                     </p>
 
-                    <p className="texto-proj">O objetivo geral é proporcionar a pessoas com deficiência intelectual a familiarização e o aprimoramento de conhecimentos em informática, contribuindo para a sua inclusão digital e, consequentemente, sua inclusão social. Não somente o projeto tem auxiliado na inserção no mercado de trabalho, mas também tem melhorado a autoestima dos alunos. O empreedimento em parceria com o IFSP e a APAE tem sido divulgado em diversos congressos e em revistas,
+                    <p className="projeto__texto">O objetivo geral é proporcionar a pessoas com deficiência intelectual a familiarização e o aprimoramento de conhecimentos em informática, contribuindo para a sua inclusão digital e, consequentemente, sua inclusão social. Não somente o projeto tem auxiliado na inserção no mercado de trabalho, mas também tem melhorado a autoestima dos alunos. O empreedimento em parceria com o IFSP e a APAE tem sido divulgado em diversos congressos e em revistas,
                         como a do Napne.
                     </p>
                 </div>
