644b289a73f391058fd36a7d5471467a3a79993e
Vitor Bonelli <bonelli5.vitor@gmail.com>
2023-10-17 22:48:17 -0300

    fix(home): fix title resizing

--- src/paginas/Home/home.js
+++ src/paginas/Home/home.js
@@ -5,19 +5,28 @@ import Banner from './banner.png';
 export default function Home({scrollToSection, closedMenuHeight}) {
     return (
         <div>
+
             <section className="home-container">
+
                 <div className="img-home">
                     <img src={Banner} alt="Img_Home" />
                 </div>
+
                 <div className="container-titulo-and-button">
-                    <h1 className="home-titulo">Atuando pela inclusão digital: uma parceria entre o IFSP <br/> e a APAE</h1>
+
+                    <h1 className="home-titulo">Atuando pela inclusão digital: uma parceria entre o IFSP e a APAE</h1>
                     <div className="titulo-button-banner">
+
                         <div className="titulo-button">
                             <button className="button-home" onClick={() => scrollToSection("projeto", closedMenuHeight)}>Conheça o Projeto</button>
                         </div>
+
                     </div>
+
                 </div>
+
             </section>
+
         </div>
     )
 }
