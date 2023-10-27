0b3285e51972c5f4d52b1cdadf658c26aa3bdb77
BBranco97 <barbarabranco1997@gmail.com>
2023-10-26 21:37:55 -0300

    mais padronizações
    botoes e select / cards

--- src/componentes/Cards/Card_Acoes/card-acoes.js
+++ src/componentes/Cards/Card_Acoes/card-acoes.js
@@ -7,19 +7,17 @@ function Card({ title, imageUrl, body, handleOpenModal, cardId }) {
         <div className="Card">
 
         <div className = "img-titulo-body-container">
-            <div className="image-container">
+            <div>
                 <img className="imgcard" src={imageUrl} alt='img'/>
             </div>
 
-            <div className="card-content">
                 <div className="card-title">
-                    <h3 className="titulo-card">{title}</h3>
+                    <p className="titulo-card">{title}</p>
                 </div>
                 
                 <div className="card-body">
                     <p className="paragrafo-card">{body}</p>
                 </div>
-            </div>
         </div>
             <div className="div-button">
                 <button onClick={() => handleOpenModal(cardId)}>Galeria de Fotos</button>
