6218a355dbe84116419c724b8e3018511b345f4b
Bianca <bianca.carvalio@aluno.ifsp.edu.br>
2023-11-16 14:44:08 -0300

    inserindo layout em landscape / quando o celular vira na horizontal

--- src/componentes/layout/MenuNav/MenuNav.js
+++ src/componentes/layout/MenuNav/MenuNav.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './MenuNav.module.css';
 import logo from './logo_navheader.png';
 import burguerIcon from './burguericon.png';
